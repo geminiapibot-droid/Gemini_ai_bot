44023d11cbb41ebc71bb8ef0f9bb3142cfb3e441
borushiki001 <borushiki872007@gmail.com>
2026-04-08 11:53:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Pyrogram setup
-API_ID = "12345678"  # Replace this API ID with your actual API ID
-API_HASH = "XXXXXXXXXXXXXX"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "XXXXXXXXXXXXXXXXXXX"  # Replace this BOT_TOKEN
+API_ID = "31476539"  # Replace this API ID with your actual API ID
+API_HASH = "1fe6cd52cde03cdc0c2571ed750c19f4"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8704218693:AAFvEBFPzO19yFHylXlcE-qj9NxQ-1GZZcU"  # Replace this BOT_TOKEN
 
 # Google Api Key
 GOOGLE_API_KEY = "XXXXXXXXXXXXXXXXXXXX"  # Replace this Google Api Key
